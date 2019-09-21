46acd914021e5285772fd4c5f7cf88e6c7c1d828
Pawan Jain <jainpawan211199@gmail.com>
2019-09-21 15:10:39 +0530

    added initial login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import {Route, Switch, Redirect} from 'react-router-dom'
-import NavBar from './components/NavBar.js'
+import NavBar from './components/NavBar'
+import Login from './pages/Login'
 function App() {
   return (
     <div>
       <>
         <NavBar />
+        <Switch>
+          <Route path="/login" render={() => <Login />} />
+        </Switch>
       </>
     </div>
   );
