33e2363e9f2257c348401ef9b6d1c71d2a42a869
Pawan Jain <jainpawan211199@gmail.com>
2019-09-21 18:18:44 +0530

    added stationadd

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar'
 import Login from './pages/Login'
 import CaseList from './pages/CaseList'
 import CaseAdd from './pages/CaseAdd'
-
+import StationAdd from './pages/StationAdd'
 function App() {
   function requireCheck() {
 		if (localStorage.getItem("token")) {
@@ -21,6 +21,7 @@ function App() {
           <Route path="/login" render={(props) => requireCheck() ? <Redirect to="/" /> : <Login {...props}/> } />
           <Route path="/" exact render={(props) => requireCheck() ? <CaseList {...props}/> : <Redirect to="/login" /> }/>
           <Route path="/add" render={(props) => requireCheck() ? <CaseAdd {...props}/> : <Redirect to="/login" /> } />
+          <Route path="/stationadd" render={(props) => requireCheck() ? <StationAdd {...props}/> : <Redirect to="/login" /> } />
         </Switch>
       </>
     </div>
